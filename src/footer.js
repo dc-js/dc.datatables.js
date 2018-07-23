3a33e3c906702d6e71383bc4f7dcfc7975515a45
Gordon Woodhull <gordon@woodhull.com>
2018-07-23 06:54:28 -0400

    correct module name/deps

--- src/footer.js
+++ src/footer.js
@@ -1,23 +1,15 @@
 // Expose d3 and crossfilter, so that clients in browserify
 // case can obtain them if they need them.
-dc.crossfilter = crossfilter;
+dc_datatables.crossfilter = crossfilter;
 
-return dc;}
+return dc_datatables;}
     if(typeof define === "function" && define.amd) {
-        define(["dc"], _dcdt);
+        define(["dc_datatables"], _dcdt);
     } else if(typeof module === "object" && module.exports) {
         var _dc = require('dc');
-        // When using npm + browserify, 'crossfilter' is a function,
-        // since package.json specifies index.js as main function, and it
-        // does special handling. When using bower + browserify,
-        // there's no main in bower.json (in fact, there's no bower.json),
-        // so we need to fix it.
-        if (typeof _crossfilter !== "function") {
-            _crossfilter = _crossfilter.crossfilter;
-        }
-        module.exports = _dcdt(_d3, _crossfilter);
+        module.exports = _dcdt(_dc);
     } else {
-        this.dc = _dcdt(d3, crossfilter);
+        this.dc_datatables = _dcdt(dc);
     }
 }
 )();
