0192862ae6d4e08c6d84fd7c42985254d02726da
Gordon Woodhull <gordon@woodhull.com>
2019-06-05 10:18:46 -0400

    upgrade, move to eslint, and remove dead dependencies
    for a clean audit. note: doc build chain is partly missing but so are docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,28 +41,18 @@ module.exports = function (grunt) {
                 dest: '<%= conf.pkg.name %>.min.js'
             }
         },
-        jscs: {
-            old: {
-                src: ['<%= conf.spec %>/**/*.js'],
-                options: {
-                    validateIndentation: 4
-                }
+        eslint: {
+            source: {
+                src: [
+                    '<%= conf.src %>/**/*.js',
+                    '!<%= conf.src %>/{banner,footer,d3v3-compat}.js',
+                    '<%= conf.spec %>/**/*.js',
+                    'Gruntfile.js',
+                    'grunt/*.js'
+                ]
             },
-            source: {
-                src: ['<%= conf.src %>/**/*.js', '!<%= conf.src %>/{banner,footer}.js', 'Gruntfile.js',
-                      'grunt/*.js', '<%= conf.web %>/stock.js'],
-                options: {
-                    config: '.jscsrc'
-                }
-            }
-        },
-        jshint: {
-            source: {
-                src: ['<%= conf.src %>/**/*.js', 'Gruntfile.js', 'grunt/*.js', '<%= conf.web %>/stock.js'],
-                options: {
-                    jshintrc: '.jshintrc',
-                    ignores: ['<%= conf.src %>/banner.js', '<%= conf.src %>/footer.js']
-                }
+            options: {
+                configFile: '.eslintrc'
             }
         },
         watch: {
@@ -104,13 +94,6 @@ module.exports = function (grunt) {
                 src: '<%= emu.api.dest %>',
                 dest: '<%= emu.api.dest %>'
             }
-        },
-        markdown: {
-            html: {
-                src: '<%= emu.api.dest %>',
-                dest: '<%= conf.web %>/docs/index.html'
-            },
-            options: {markdownOptions: {highlight: 'manual'}}
         },
         copy: {
             'dc-to-gh': {
@@ -221,22 +204,6 @@ module.exports = function (grunt) {
         grunt.log.writeln('Merge Github Pull Request #' + pr);
         grunt.task.run(['shell:merge:' + pr, 'test' , 'shell:amend']);
     });
-    grunt.registerMultiTask('toc', 'Generate a markdown table of contents.', function () {
-        var marked = require('marked'),
-            slugify = function (s) { return s.trim().replace(/[-_\s]+/g, '-').toLowerCase(); },
-            srcFile = this.files[0].src[0],
-            destFile = this.files[0].dest,
-            source = grunt.file.read(srcFile),
-            tokens = marked.lexer(source),
-            toc = tokens.filter(function (item) {
-                return item.type === 'heading' && item.depth === 2;
-            }).reduce(function (toc, item) {
-                return toc + '  * [' + item.text + '](#' + slugify(item.text) + ')\n';
-            }, '');
-
-        grunt.file.write(destFile, '# dc.datatables.js API\n' + toc + '\n' + source);
-        grunt.log.writeln('Added TOC to \'' + destFile + '\'.');
-    });
     grunt.registerTask('test-stock-example', 'Test a new rendering of the stock example web page against a ' +
                        'baseline rendering', function (option) {
                            require('./regression/stock-regression-test.js').testStockExample(this.async(), option === 'diff');
@@ -247,7 +214,7 @@ module.exports = function (grunt) {
 
     // task aliases
     grunt.registerTask('build', ['concat', 'uglify']);
-    grunt.registerTask('docs', ['build', 'copy', 'emu', 'toc', 'markdown']);
+    grunt.registerTask('docs', ['build', 'copy', 'emu']);
     grunt.registerTask('web', ['docs', 'gh-pages']);
     grunt.registerTask('server', ['docs', 'connect:server', 'watch:scripts']);
     grunt.registerTask('test', ['build', 'jasmine:specs', 'shell:hooks']);
@@ -255,7 +222,7 @@ module.exports = function (grunt) {
     grunt.registerTask('coverage', ['build', 'jasmine:coverage']);
     grunt.registerTask('ci', ['test', 'jasmine:specs:build', 'connect:server', 'saucelabs-jasmine']);
     grunt.registerTask('ci-pull', ['test', 'jasmine:specs:build', 'connect:server']);
-    grunt.registerTask('lint', ['build', 'jshint', 'jscs']);
+    grunt.registerTask('lint', ['build', 'eslint']);
     grunt.registerTask('default', ['build']);
 };
 
