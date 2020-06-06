ac229a9f3f48c847c4846420670f360de3b0a927
Gordon Woodhull <gordon@woodhull.com>
2020-06-06 11:11:32 -0400

    Synced from from master branch.

--- js/dc.datatables.js
+++ js/dc.datatables.js
@@ -1,5 +1,5 @@
 /*!
- *  dc.datatables 0.0.7
+ *  dc.datatables 0.0.8
  *  http://dc-js.github.io/dc.datatables.js/
  *  Copyright 2018 Gordon Woodhull & the dc.datatables Developers
  *  https://github.com/dc-js/dc.datatables.js/blob/master/AUTHORS
@@ -21,9 +21,39 @@
 
 /*jshint -W079*/
 var dc_datatables = {
-    version: '0.0.7'
+    version: '0.0.8'
 };
 
+// Object.assign polyfill for IE
+if (typeof Object.assign != 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, "assign", {
+    value: function assign(target, varArgs) { // .length of function is 2
+      'use strict';
+      if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var to = Object(target);
+
+      for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+          for (var nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    writable: true,
+    configurable: true
+  });
+}
 
 dc_datatables.datatable = function(selector, chartGroup) {
     var _table = {}, // this object
